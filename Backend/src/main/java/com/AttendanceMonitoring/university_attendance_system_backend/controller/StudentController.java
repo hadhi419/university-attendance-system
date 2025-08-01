c902b477d8b87667425688ae3ca3944ee47f71f1
Mohomed Hadhi <dev.hadhi419@gmail.com>
2025-08-01 16:55:07 +0530

    Update StudentController.java

--- Backend/src/main/java/com/AttendanceMonitoring/university_attendance_system_backend/controller/StudentController.java
+++ Backend/src/main/java/com/AttendanceMonitoring/university_attendance_system_backend/controller/StudentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/students")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://university-attendance-system-git-feat-5018fb-hadhi419s-projects.vercel.app")
 public class StudentController {
 
     @Autowired
