e89d6dd212c8ee9f72c70c62509df33138351803
hadhi419 <dev.hadhi419@gmail.com>
2025-08-07 09:41:24 +0530

    Fixed the jwt bug

--- frontend/frontend/src/components/Navbar.jsx
+++ frontend/frontend/src/components/Navbar.jsx
@@ -15,14 +15,14 @@ const Navbar = ({ setIsAuthenticated }) => {
     <header className="bg-cyan shadow px-4 py-3 w-full flex justify-between items-center h-15">
       <h1 className="text-xl font-bold text-cyan-800">Attendance System</h1>
       <ul className="flex space-x-5">
-        <li>
+        {/* <li>
           <Link to="/" className="text-blue-600 hover:underline">Dashboard</Link>
         </li>
         <li>
           <Link to="/admin" className="text-blue-600 hover:underline">Admin Panel</Link>
-        </li>
+        </li> */}
         <li>
-          <button onClick={handleLogout} className="text-blue-600 hover:underline bg-none border-none p-0 text-left">
+          <button onClick={handleLogout} className="text-blue-900 hover:underline bg-none border-none p-0 text-left">
             Logout
           </button>
         </li>
